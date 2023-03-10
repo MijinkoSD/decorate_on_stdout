bf31f25e1eac5a63b352152a9b620b2527f9c615
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2023-03-11 00:31:44 +0900

    いろいろ変えた

---
+++ decorate_on_stdout/__init__.py
@@ -0,0 +1,9 @@
+from decorate_on_stdout.decorate import Deco
+
+__version__ = "0.1.0"
+__author__ = "MijinkoSD"
+__url__ = "https://github.com/MijinkoSD/decorate_on_stdout"
+
+__all__ = [
+    "Deco"
+]
