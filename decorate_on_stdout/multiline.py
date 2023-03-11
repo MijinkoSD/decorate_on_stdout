24be93869ec5b495742f7f8b0dc09b63e867e985
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2023-03-12 02:00:13 +0900

    複数行のコンソールテキストを動的に書き換える機能を追加

---
+++ decorate_on_stdout/multiline.py
@@ -0,0 +1,114 @@
+import sys
+from asyncio import sleep, run
+
+ESC = "\033["
+
+
+class _Line:
+    def __init__(self, multi_line, text: str = ""):
+        self.multi_line = multi_line
+        self.text = text
+
+    # def __del__(self):
+    #     self.multi_line.remove_line(line=self)
+
+    async def update(self, text: str):
+        """ 表示内容を更新します """
+        self.text = text
+        await self.multi_line.update_line(text=text, line=self)
+    
+    async def remove(self):
+        """ 行を削除します """
+        await self.multi_line.remove_line(self)
+        del self
+
+class MultiLine:
+    def __init__(self):
+        """ コンソールテキストの複数行の更新をサポートします。 """
+        self.lines: list[_Line] = []
+        """ 行ごとのインスタンス。添字が若いほど上の行。 """
+        self.allow_remove_line = True
+        """ 行の削除を許可するか """
+
+    def __del__(self):
+        self.allow_remove_line = False
+
+    async def add_line(self, text: str = "") -> _Line:
+        """ 表示する行を下に追加します。 """
+        line = _Line(self, text=text)
+        self.lines.append(line)
+        sys.stdout.write("\n" + text)
+        return line
+
+    async def update_line(self, text: str, line: _Line) -> None:
+        try:
+            index = self.lines.index(line)
+        except IndexError:
+            return
+        
+        count = len(self.lines)
+        move_len = count - index - 1
+        self._cursor_up(move_len)
+        self._render_cursor_line(text)
+        self._cursor_down(move_len)
+
+        bottom_line_text = self.lines[-1].text
+        self._render_cursor_line(bottom_line_text)
+
+    async def remove_line(self, line: _Line) -> None:
+        """ 行を削除します。 """
+        if not self.allow_remove_line: return
+        try:
+            index = self.lines.index(line)
+        except IndexError:
+            return
+        
+        count = len(self.lines)
+        move_len_min = count - index - 1
+        self._cursor_up(move_len_min)
+        for i in range(index - 1, -1, -1):
+            self._render_cursor_line(self.lines[i].text)
+            self._cursor_up(1)
+        self._render_cursor_line("")
+        self._cursor_down(count - 1)
+        bottom_line_text = self.lines[-1].text
+        self._render_cursor_line(bottom_line_text)
+
+        self.lines.pop(index)
+
+    @staticmethod
+    def _cursor_up(count: int) -> None:
+        sys.stdout.write(f"{ESC}{count}F")
+
+    @staticmethod
+    def _cursor_down(count: int) -> None:
+        sys.stdout.write(f"{ESC}{count}E")
+
+    @staticmethod
+    def _render_cursor_line(text: str) -> None:
+        sys.stdout.write(f"{ESC}2K")
+        sys.stdout.write("\r" + text)
+
+
+async def _main():
+    """ 本来ここに書くべきではないが、面倒くさかったので…（後で消す） """
+
+    m = MultiLine()
+    l1 = await m.add_line("sample1")
+    l2 = await m.add_line("sample2")
+    l3 = await m.add_line("sample3")
+    l4 = await m.add_line("sample4")
+    await sleep(0.5)
+    await l2.update("oooo")
+    await sleep(0.5)
+    await l2.update("aaaa")
+    await sleep(0.5)
+    await l2.remove()
+    await sleep(0.5)
+    await l3.update("sample?")
+    await sleep(0.5)
+
+if __name__ == "__main__":
+    # TODO: テストコードとして分割する
+
+    run(_main())
