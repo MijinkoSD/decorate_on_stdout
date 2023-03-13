7687f7797b1755ce8e0e3c6a938b1e7f30c1275d
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2023-03-14 02:43:52 +0900

    Add: `decorate.py`のテストファイルを追加

---
+++ test/test_decorate.py
@@ -0,0 +1,20 @@
+from typing import Final
+import unittest
+from stdoutdeco import Deco as D
+
+
+class TestDeco(unittest.TestCase):
+    def test_decorate(self):
+        assert True, "試しに失敗させてみる"
+
+    def test_black(self):
+        funcs = [D.black, D.red, D.green, D.yellow,
+                 D.blue, D.magenta, D.cyan, D.white]
+        codes = [30, 31, 32, 33, 34, 35, 36, 37]
+
+        TEXT: Final[str] = "Sample Text"
+        ESC: Final[str] = "\033["
+        for func, code in zip(funcs, codes, strict=True):
+            colored = func(TEXT)
+            right = ESC + str(code) + "m" + TEXT + ESC + "0m"
+            assert right == colored, f'"{colored}" is must be "{right}".'
